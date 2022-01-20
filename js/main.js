39a8b219f0c7b074f9001d738d3d891d0be4e7f2
riansyah <mriansyah93@gmail.com>
2022-01-20 17:48:32 +0800

    fix grid of projects

--- js/main.js
+++ js/main.js
@@ -3,20 +3,32 @@
  * Copyright Mehdi TAZI 2014
  */
 $(function() {
-	
 	//OnLoad
-	$(document).ready(function() {
 	
-				$('.skillsPieChart').radarChart({
-				size: [380, 300],
-				step: 1,
-				fixedMaxValue:5,
-				showAxisLabels: true
-				});
-	
-    });
 
+  //   function generate(){
+		// const mainCont = document.createDocumentFragment();		    	
+		// projects.forEach(function(obj) { 
+		// 	// perhatikan susunan div nya!
+		//    const penBubWra = document.createElement('div');
+		//    penBubWra.className = 'bubbleWrapper';
+		//    mainCont.appendChild(penBubWra);
+	 //    });
+	 //    document.body.appendChild(mainCont);
+  //   }
+  //   const projects = [
+  //   	{'':''}
+  //   ]
+  //   generate(projects);
+    // nested array nih, dia hanya bisa nampung tiga 
 });
 
 
+
+// <div class="flex-grow-1">
+//     <img src="./assets/img/gimsil.png" class="img-thumbnail projectImage">
+//     <h3 class="mb-0">Self Chat</h3>
+//     <p>Using Front-end Technology</p>                            
+//     <p><a target="_blank" href="https://riansyahtohamba.github.io/creative/chats/badri.html">Live Preview</a></p>
+// </div>
 
