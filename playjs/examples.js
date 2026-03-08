a2d1482fd1c240884df5c6a11cc3e27967b78185
RiansyahTohamba <mriansyah93@gmail.com>
2026-03-09 07:50:28 +0800

    add playground js

---
+++ playjs/examples.js
@@ -0,0 +1,17 @@
+const examples = {
+    "Aljabar Linear": [
+        "vector.html",
+    ],
+    "Citra Digital": [
+        "image_filter.html",
+        // "(belum)histogram.html",
+        // "(belum)convolution.html",
+    ],
+    "Payroll": [
+        "pns_payroll.html",
+    ],
+    "SNBP": [
+        "kalkulator_prestasi.html",
+        "distribusi_daerah.html"
+    ]
+};
